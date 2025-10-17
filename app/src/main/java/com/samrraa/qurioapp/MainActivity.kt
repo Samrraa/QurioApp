76fe418d3b3445fd782c2462f0b9f64199479283
fatmahgazy <143911324+fatmahgazy@users.noreply.github.com>
2025-10-17 16:43:05 +0200

    feat: Add loading time animation

--- app/src/main/java/com/samrraa/qurioapp/MainActivity.kt
+++ app/src/main/java/com/samrraa/qurioapp/MainActivity.kt
@@ -1,20 +1,18 @@
 package com.samrraa.qurioapp
 
 import android.os.Bundle
-import androidx.fragment.app.FragmentActivity
-import com.samrraa.TimeFragment
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.samrraa.qurioapp.databinding.ActivityMainBinding
 
-class MainActivity : FragmentActivity() {
-
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        // Load TimeFragment
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.nav_host_fragment, TimeFragment())
-                .commit()
-        }
+        installSplashScreen()
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
