238d3abbea9945df3ec4bde1a1e5bdc873a69302
fatmahgazy <143911324+fatmahgazy@users.noreply.github.com>
2025-10-17 16:41:16 +0200

    feat: Add loading time animation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,5 +52,4 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.core.splashscreen)
-    implementation(libs.gif.drawable)
 }
--- app/src/main/java/com/samrraa/qurioapp/MainActivity.kt
+++ app/src/main/java/com/samrraa/qurioapp/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.samrraa.qurioapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.samrraa.qurioapp.databinding.ActivityMainBinding
+import androidx.fragment.app.FragmentActivity
+import com.samrraa.TimeFragment
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class MainActivity : FragmentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen()
-        enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
+
+        // Load TimeFragment
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.nav_host_fragment, TimeFragment())
+                .commit()
+        }
     }
 }
